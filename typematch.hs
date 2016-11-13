88a57cda7cd4e994bcd2ef2cbae216fceb3488f4
pasaunders <kaorti@gmail.com>
2016-11-13 14:30:40 -0800

    type matching exercise

---
+++ typematch.hs
@@ -0,0 +1,42 @@
+module TypeMatch where
+
+import Data.List(sort)
+
+mySort :: [Char] -> [Char]
+mySort = sort
+
+signifier :: Ord a => [a] -> a
+signifier xs = head (mySort xs)
+
+-- young :: [Char] -> Char
+-- young xs = head (sort xs)
+--
+-- jung :: [Int] -> Int
+-- jung xs = head (sort xs)
+
+-- myX = 1:: Int
+-- sigmund' :: Num a => a -> a
+-- sigmund' x = myX
+
+-- myX = 1 :: Int
+--
+-- sigmund :: Int -> Int
+-- sigmund x = myX
+
+-- freud' :: Int -> Int
+-- freud' x = x
+
+-- freud :: Ord a => a -> a
+-- freud x = x
+
+-- f :: RealFrac a => a
+-- f = 1.0
+
+-- f :: Fractional a => a
+-- f = 1.0
+
+-- f :: Num a => a
+-- f = 1.0
+
+-- i :: Num a => a
+-- i = 1
