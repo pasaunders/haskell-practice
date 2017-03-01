d5001c5f921a452dedbb53bd3be006a2440b2dbb
pasaunders <kaorti@gmail.com>
2017-02-28 18:50:11 -0800

    more case expression practice

--- CaseExpressions.hs
+++ CaseExpressions.hs
@@ -1,23 +1,32 @@
 module CaseExpressions where
 
 funcZ :: Integer -> String
-funcZ = case x + 1 == 1 of
+funcZ = 
+  case x + 1 == 1 of
   True -> "AWESOME"
   False -> "wut"
 
-pal :: List -> String
+pal :: String -> String
 pal xs =
   case xs == reverse xs of
     True -> "yes"
     False -> "no"
 
-pal' :: List -> String
+pal' :: String -> String
 pal' xs =
   case y of
     True -> "yes"
     False -> "no"
   where y = xs == reverse xs
 
-funcC ::
+funcC :: a-> b -> Either a b
 funcC x y =
-  case
+  case x > y of
+    True -> x
+    False -> y
+
+ifEvenAdd2 :: Num a => a -> a
+ifEvenAdd2 n =
+  case mod n 2 == 0 of
+    True -> n + 2
+    False -> n
