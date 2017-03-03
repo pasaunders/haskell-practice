1b92f84c278550c80262b35b9ca92637d844c7b4
pasaunders <kaorti@gmail.com>
2017-03-02 18:38:32 -0800

    fixed errors with type signatures. finished case expressions exercises.

--- CaseExpressions.hs
+++ CaseExpressions.hs
@@ -1,7 +1,7 @@
 module CaseExpressions where
 
 funcZ :: Integer -> String
-funcZ = 
+funcZ x = 
   case x + 1 == 1 of
   True -> "AWESOME"
   False -> "wut"
@@ -19,14 +19,21 @@ pal' xs =
     False -> "no"
   where y = xs == reverse xs
 
-funcC :: a-> b -> Either a b
+funcC :: Ord a => a -> a -> a
 funcC x y =
   case x > y of
     True -> x
     False -> y
 
-ifEvenAdd2 :: Num a => a -> a
+ifEvenAdd2 :: Integral a => a -> a
 ifEvenAdd2 n =
   case mod n 2 == 0 of
     True -> n + 2
     False -> n
+
+nums :: (Num a, Ord a) => a -> Integer
+nums x =
+  case compare x 0 of
+    LT -> -1
+    GT -> 1
+    EQ -> 0
