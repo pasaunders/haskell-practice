fd23aba52a0c1ca8e20c5f7805fb27fb8a73f73a
pasaunders <kaorti@gmail.com>
2017-04-24 21:21:41 -0700

    recursively composing a function n times.

--- factorial.hs
+++ factorial.hs
@@ -9,3 +9,13 @@ brokenFact1 n = n * brokenFact1 (n - 1)
 factorial :: Integer -> Integer
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
+
+ -- takes two arguments - starting number and number of times to increment.
+incTimes :: (Eq a, Num a) => a -> a -> a
+incTimes 0 n = n
+incTimes times n = 1 + (incTimes (times - 1) n)
+
+-- recursively composes function f with applyTimes n times.
+applyTimes :: (Eq a, Num a) => a -> (b -> b) -> b -> b
+applyTimes 0 f b = b
+applyTimes n f b = f . applyTimes (n - 1) f $ b
