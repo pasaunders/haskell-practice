bc208ea5e3ccf2c5a4b5fc1b7d15dd0c95d1f810
pasaunders <kaorti@gmail.com>
2017-04-26 15:46:45 -0700

    recursion exercises

--- bottom.hs
+++ bottom.hs
@@ -4,22 +4,33 @@ f :: Bool -> Int
 f True = error "blah"
 f False = 0
 
-data Maybe a = Nothing | Just a
-
-f :: Bool -> Maybe Int
-f False = Just 0
-f _ = Nothing
+g :: Bool -> Maybe Int
+g False = Just 0
+g _ = Nothing
 
 fibonacci :: Integer -> Integer
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci x = fibonacci (x - 1) + fibonacci (x - 2)
 
+data DividedResult =
+  Result Integer | DividedByZero
 
 
-
-dividedBy :: Integral a => a-> a-> (a, a)
+-- ask ben how to do all this, see textbook pg 293
+dividedBy :: Integral a => a -> a -> (a, a)
 dividedBy num denom = go num denom 0
   where go n d count
-        | n < d = (count, n)
-        | otherwise = go (n - d) d (count + 1)
+          | d == 0 = DividedByZero
+          | n < d = (count, n)
+          | otherwise = go (n - d) d (count + 1)
+
+lineAddition :: (Eq a, Num a) => a -> a
+lineAddition 0 = 0
+lineAddition n = n + lineAddition(n - 1)
+
+multiplyBy :: Integral a => a -> a ->a
+multiplyBy factorOne factorTwo = go 0 factorTwo 0
+  where go factor target count
+          | count == target = factor
+          | otherwise = go (factor + factorOne) target (count + 1)
