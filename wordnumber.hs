41d7fd74acab8f967e54dd1d26f87e37cc22a1b6
pasaunders <kaorti@gmail.com>
2017-04-27 15:13:56 -0700

    exercise to convert a number into the english words for each digit

---
+++ wordnumber.hs
@@ -0,0 +1,12 @@
+module WordNumber where
+
+import Data.List (intersperse)
+
+digtToWord :: Int -> String
+digtToWord n = undefined
+
+digits :: Int -> [Int]
+digits n = undefined
+
+wordNumber :: Int -> String
+wordNumber n = undefined
