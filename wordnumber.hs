a7e0632f089d62c3eddea9a2a0abefe23138c333
pasaunders <kaorti@gmail.com>
2017-04-27 16:24:21 -0700

    module loads, but digits function exceeds stack depth, recursion is incorrect

--- wordnumber.hs
+++ wordnumber.hs
@@ -2,11 +2,20 @@ module WordNumber where
 
 import Data.List (intersperse)
 
-digtToWord :: Int -> String
-digtToWord n = undefined
+digitToWord :: Int -> String
+digitToWord 1 = "one"
+digitToWord 2 = "two"
+digitToWord 3 = "three"
+digitToWord 4 = "four"
+digitToWord 5 = "five"
+digitToWord 6 = "six"
+digitToWord 7 = "seven"
+digitToWord 8 = "eight"
+digitToWord 9 = "nine"
+digitToWord 0 = "zero"
 
 digits :: Int -> [Int]
-digits n = undefined
+digits n = digits (div n 10) ++ [mod n 10]
 
 wordNumber :: Int -> String
-wordNumber n = undefined
+wordNumber n = concat (intersperse "-" (map digitToWord (digits n)))
