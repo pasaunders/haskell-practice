1625a0a76d7e4fee19c08ba0db93752f7198e9da
pasaunders <kaorti@gmail.com>
2017-04-30 14:30:00 -0700

    implemented my own enumFromTo functions

--- list.hs
+++ list.hs
@@ -4,6 +4,23 @@ eftBool :: Bool -> Bool -> [Bool]
 eftBool False True =  [False, True]
 eftBool False False = [False]
 eftBool True True = [True]
+eftBool _ _ = []
 
 eftOrd :: Ordering -> Ordering -> [Ordering]
-eftOrd =
+eftOrd LT EQ = [LT, EQ]
+eftOrd LT LT = [LT]
+eftOrd LT GT = [LT, EQ, GT]
+eftOrd EQ GT = [EQ, GT]
+eftOrd EQ EQ = [EQ]
+eftOrd GT GT = [GT]
+eftOrd _ _  = []
+
+eftInt :: Int -> Int -> [Int]
+eftInt x y
+  | x == y = [y]
+  | otherwise = x : eftInt (x + 1) y
+
+eftChar :: Char -> Char -> [Char]
+eftChar x y
+  | x == y = [y]
+  | otherwise = x : eftChar (succ x) y
