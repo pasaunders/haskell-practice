5e881da0f2ae50c2deb61f5527bb43387efab65e
pasaunders <kaorti@gmail.com>
2017-05-04 22:18:04 -0700

    simple introduction to map in haskell

--- takedrop.hs
+++ takedrop.hs
@@ -20,19 +20,19 @@ rvrs x = let
   in concat [awe, " ", is, " ", cur]
 
 mySqr :: Int -> [Int]
-mSqr x = [x^2 | x <- [1..5]]
+mySqr x = [x^2 | x <- [1..5]]
 
 myCube :: Int -> [Int]
 myCube x = [x^3 | x <- [1..5]]
 
-[x | x <- mySqr, rem x 2 == 0]
-[(x, y) | x <- mySqr, y <- mySqr, x < 50, y > 50]
-take 5 [ (x, y) | x <- mySqr, y <- mySqr, x < 50, y > 50]
+-- [x | x <- mySqr, rem x 2 == 0]
+-- [(x, y) | x <- mySqr, y <- mySqr, x < 50, y > 50]
+-- take 5 [ (x, y) | x <- mySqr, y <- mySqr, x < 50, y > 50]
 
-[(x, y) | x <- mySqr, y <- myCube]
-[(x, y) | x <- mySqr, y <- myCube, x < 50, y < 50]
+-- [(x, y) | x <- mySqr, y <- myCube]
+-- [(x, y) | x <- mySqr, y <- myCube, x < 50, y < 50]
 
-length [(x, y) | x <- mySqr, y <- myCube, x < 50, y < 50]
+-- length [(x, y) | x <- mySqr, y <- myCube, x < 50, y < 50]
 
 myLength :: [a] -> Integer
 myLength [] = 0
@@ -41,3 +41,8 @@ myLength (_:xs) = 1 + myLength xs
 mySum :: Num a => [a] -> a
 mySum [] = 0
 mySum (x : xs) = x + mySum xs
+
+-- map (\x -> if x == 3 then (-x) else (X)) [1..10]
+
+mysteryFunction :: String -> [Bool]
+mysteryFunction xs = map (\x -> elem x "aeiou") xs
