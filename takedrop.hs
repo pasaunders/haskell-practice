064abb4785ab65e814bf5559902984820a9b48d9
pasaunders <kaorti@gmail.com>
2017-05-05 19:35:56 -0700

    filtering, zipping and unzipping practice

--- takedrop.hs
+++ takedrop.hs
@@ -1,5 +1,7 @@
 module TakeDrop where
 
+import Data.Char
+
 takeFunction :: String -> String
 takeFunction x = take 16 x
 
@@ -46,3 +48,29 @@ mySum (x : xs) = x + mySum xs
 
 mysteryFunction :: String -> [Bool]
 mysteryFunction xs = map (\x -> elem x "aeiou") xs
+
+
+myFilter :: [Int] -> [Int]
+myFilter x = filter (\y -> (mod y 3) == 0) x   -- ask ben about non-lambda alternatives
+
+numFiltered :: [Int] -> Int
+numFiltered = length . myFilter
+
+sentenceFilter :: String -> [String]
+sentenceFilter x = filter  (\y -> not (y == "a" || y == "an" || y == "the")) (words x)
+
+myZip :: [a] -> [b] -> [(a, b)]
+myZip _ [] = []
+myZip [] _ = []
+myZip (x : xs) (y : ys) = (x, y) : myZip xs ys
+
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith _ [] _ = []
+myZipWith _ _ [] = []
+myZipWith f (x : xs) (y : ys) = f x y : myZipWith f xs ys
+
+upperFilter :: String -> String
+upperFilter x = filter (\y -> not (isUpper y)) x
+
+lowerFilter :: String -> String
+lowerFilter x = filter isUpper x
