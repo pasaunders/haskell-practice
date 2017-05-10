5a4926ee8a962c35ebbdf42975acc8f7bd303bb0
pasaunders <kaorti@gmail.com>
2017-05-09 19:39:31 -0700

    begin exercises to demonstrate foldl and foldr

--- folding.hs
+++ folding.hs
@@ -9,3 +9,5 @@ myFoldr f z xs =
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
 myFoldl f acc []     = acc
 myFoldl f acc (x:xs) = myFoldl f (f acc x) xs
+
+foldl (flip (*)) 1 [1..5] == (5 * (4 * (3 * (2 * (1 * 1)))))
